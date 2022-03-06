014f2b68bc2d5491d41a4fc4e45c029da1fd4e88
Amulya Arun <amulyaarun17@gmail.com>
2022-03-06 21:03:59 +0530

    added routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -10,31 +12,34 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService: AccountService, private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
-  login():void {
-    this.accountService.login(this.model).subscribe(response => 
-    {
+  login(): void {
+    this.accountService.login(this.model).subscribe(response => {
       console.log(response);
+      this.router.navigateByUrl('/members');
     },
-    error =>{
-      console.log(error);
-    });
+      error => {
+        console.log(error);
+        this.toastr.error(error.error);
+      });
   }
 
   logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
-/*   getCurrentUser(){
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user;
-    },
-    error => {
-      console.log(error);
-    });
-  } */
+  /*   getCurrentUser(){
+      this.accountService.currentUser$.subscribe(user => {
+        this.loggedIn = !!user;
+      },
+      error => {
+        console.log(error);
+      });
+    } */
 }
