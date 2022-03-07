879b2852eeb31c9df78e8856133a881b75a30cec
Amulya Arun <amulyaarun17@gmail.com>
2022-03-07 14:16:13 +0530

    Error Handling

--- API/Startup.cs
+++ API/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using API.Extensions;
+using System.Collections.Generic;
+using API.Middleware;
 
 namespace API
 {
@@ -21,9 +23,35 @@ namespace API
         {
             services.AddApplicationServices(_config);
             services.AddControllers();
+            //Adding Secutity Definition: to get that Authorize section in swagger DOC
+            //Adding Secutity Requirement: to get that pop up to enter token as 'Bearer 'token name', this will add Authorize to every endpoint call in header
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List<string>()
+                }
+                });
             });
             services.AddIdentityServices(_config);
             services.AddCors();
@@ -32,9 +60,9 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
