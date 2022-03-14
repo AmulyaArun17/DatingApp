6ee1c314d0f23cefe20e68a9fd67725ebed46c57
Amulya Arun <amulyaarun17@gmail.com>
2022-03-14 23:58:09 +0530

    saving updates

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -36,6 +37,20 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDTO>> GetUserByUserName(string userName)
         {
             return await _repo.GetMemberAsync(userName);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDTO)
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            AppUser user = await _repo.GetUserByNameAsync(userName);
+            _mapper.Map(memberUpdateDTO, user);
+            _repo.Update(user);
+            if (await _repo.SaveAllAsync())
+            {
+                return NoContent();
+            }
+            return BadRequest();
         }
     }
 }
